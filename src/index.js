b949edcfc75d324f2fcf69ef73ae60b8be2f22bf
Gabriel Pillegand <gabriel.pillegand@epita.fr>
2025-08-03 18:35:04 +0200

    bot is up !

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js')
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log("The bot is ready !")
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
