2f239f79a6db8817237a3cba33b9bce92b5bf131
Gabriel Pillegand <gabriel.pillegand@epita.fr>
2025-08-03 19:36:56 +0200

    First slash commands.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,36 @@ const client = new Client({
 });
 
 client.on('ready', (c) => {
-    console.log("The bot is ready !")
+    console.log(`${c.user.tag} is ready !`)
+});
+
+client.on(`messageCreate`, (message) => {
+    if (message.author.bot) {
+        return
+    }
+    if (message.content === 'hello' || message.content === 'hi' || message.content === 'coucou' || message.content === 'salut' || message.content === 'bonjour') {
+        message.react('👋')
+    }
+});
+
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+    
+    if (interaction.commandName === 'hey') {
+        interaction.reply('hey');
+    } 
+    if (interaction.commandName === 'ping') {
+        interaction.reply('Pong');
+    }
+    if (interaction.commandName === 'add') {
+        const num1 = interaction.options.getInteger('first_number');
+        const num2 = interaction.options.getInteger('second_number');
+        if (num1 !== null && num2 !== null) {
+            interaction.reply(`${num1 + num2}`);
+        } else {
+            interaction.reply('Please provide two numbers.');
+        }
+    }
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
---
+++ src/register-commands.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+const { REST, Routes, ApplicationCommandOptionType } = require('discord.js');
+
+const commands = [
+    {
+        name: 'hey',
+        description: 'Replies with hey !',
+    },
+    {
+        name: 'ping',
+        description: 'Replies with Pong !',
+    },
+    {
+        name: 'add',
+        description: 'Adds two numbers.',
+        options: [
+            {
+                name: 'first_number',
+                description: 'The first number.',
+                type: ApplicationCommandOptionType.Integer,
+                required: true,
+            },
+            {
+                name: 'second_number',
+                description: 'The second number.',
+                type: ApplicationCommandOptionType.Integer,
+                required: true,
+            },
+        ],
+    },
+    
+];
+
+const rest = new REST({ version: '10'}).setToken(process.env.DISCORD_BOT_TOKEN);
+
+(async () => {
+    try {
+        console.log('Registering slash commands...');
+
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands }
+        );
+        console.log(`Slash commands were registered successfully !`);
+    } catch (error) {
+        console.log(`There was an error : ${error}`);
+    }
+})();
+
